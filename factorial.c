8f0593311919bbc09262785b3da9fcafbe696639
Michael McGuinness <72993e95@opayq.com>
2016-10-15 11:09:32 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 int fact(int i) {
   unsigned int c, a = 1, j = 1, m = i;
-  if(i%2==0){
-    while(j <= i/2){
-      c = j * m;
-      a *= c;
-      m--; j++;
-    }
-  }
-  else{
-    while(j < (i+1)/2){
-      c = j * m;
-      if(j+1==m-1){
-        a = a * c * --m;
-      }
-      else{
+  if(i>0){
+    if(i%2==0){
+      while(j <= i/2){
+        c = j * m;
         a *= c;
+        m--; j++;
       }
-      m--; j++;
+    }
+    else{
+      while(j < (i+1)/2){
+        c = j * m;
+        if(j+1==m-1){
+          a = a * c * --m;
+        }
+        else{
+          a *= c;
+        }
+        m--; j++;
+      }
     }
   }
+  else if(i==0) a = 1;
+  else printf("Your input for the factorial function is invalid.\n");
   return a;
 }
+
