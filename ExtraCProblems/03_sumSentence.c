420dab92dd27aea7b7b54a37129d9358c8219ebb
Michael McGuinness <72993e95@opayq.com>
2017-02-18 22:07:27 +0000

    Update 03_sumSentence.c

--- ExtraCProblems/03_sumSentence.c
+++ ExtraCProblems/03_sumSentence.c
@@ -14,15 +14,15 @@ int main()
 
   for(i = 0, sum = 0; i < N - 1; i++)
   {
-    if(s[i] >= 'a' && s[i] <= 'z')
+    if(s[i] == 0)
+      break;
+    else if(s[i] >= 'a' && s[i] <= 'z')
       sum += s[i] - 'a' + 1;
     else if(s[i] >= 'A' && s[i] <= 'Z')
       sum += s[i] - 'A' + 1;
-    if(s[i] == 0)
-      break;
   }
 
   printf("The sum value of all the letters is %d.\n", sum);
-  
+
   return 0;
 }
