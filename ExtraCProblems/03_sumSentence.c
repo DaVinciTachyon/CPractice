53e65f74439069bf018173323529be822f9a70ac
Michael McGuinness <72993e95@opayq.com>
2017-02-18 22:06:27 +0000

    Update 03_sumSentence.c

--- ExtraCProblems/03_sumSentence.c
+++ ExtraCProblems/03_sumSentence.c
@@ -1,34 +1,28 @@
 #include <stdio.h>
-#define N 10
+#define N 100
 
 int main()
 {
 
-  int   a, z, A, Z, i, is[N], sum, isa[N], isA[N];
+  int   i,
+        is[N],
+        sum;
   char  s[N];
 
-  a = (int) ('a');
-  z = (int) ('z');
-  A = (int) ('A');
-  Z = (int) ('Z');
-
   printf("Tell me something:\n\t");
-  scanf("%s",&s);
+  scanf("%s", &s);
 
   for(i = 0, sum = 0; i < N - 1; i++)
   {
-    is[i] = (int) (s[i]);
-    if(is[i] >= a && is[i] <= z)
-    {
-      isa[i]= is[i] - a + 1;
-      sum += isa[i];
-    }
-    else if(is[i] >= A && is[i] <= Z)
-    {
-      isA[i]= is[i] - A + 1;
-      sum += isA[i];
-    }
+    if(s[i] >= 'a' && s[i] <= 'z')
+      sum += s[i] - 'a' + 1;
+    else if(s[i] >= 'A' && s[i] <= 'Z')
+      sum += s[i] - 'A' + 1;
+    if(s[i] == 0)
+      break;
   }
 
   printf("The sum value of all the letters is %d.\n", sum);
+  
+  return 0;
 }
