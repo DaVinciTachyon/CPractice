b77648b3eb644ee915830cc5d51eab7b5e9ada8a
Michael McGuinness <72993e95@opayq.com>
2018-08-05 04:55:35 +0200

    wiill it work this time

--- PascalTriangle.c
+++ PascalTriangle.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
-#include "ChooseFunc.c"
-#define M 13
-int main() {
-  int l;
-  do{
-    printf("Enter the highest order line you want: ");
-    scanf("%d",&l);
-    if(l<0) printf("There is no negative order available.\n");
-    else if(l<=M){
-      for(int j = 0; j <= l; j++){
-        for(int m = 1; m <= l - j; m++) {
-          printf("\t");
-        }
-        for (int i = 0; i <= j; i++) {
-          printf("%u\t\t",choose(j,i));
-        }
-        printf("\n\n");
-      }
-    }
-    else printf("The current limit is %d lines.\nTry Again\n",M);
-  } while (l>M||l<0);
-  return 0;
+#include <math.h>
+
+double nFac(double);
+double nCr(double, double);
+double nPr(double, double);
+
+int main()
+{
+	double x, y, r, sum;
+	int n;
+	
+	printf("x: ");
+	scanf("%lf",&x);
+	printf("y: ");
+	scanf("%lf",&y);
+	printf("n: ");
+	scanf("%lf",&n);
+	
+	for(r = 0, sum = 0; r <= n; r++)
+		sum += nCr(n, r) * pow(x, r) * pow(y, n - r);
+	
+	printf("%lf\t%lf", sum, pow(x + y, n));
+	
+	return 0;
+}
+
+double nFac(double n)
+{
+	double fac;
+	int	i;
+	
+	for(i = 1, fac = 1; i <= n; i++)
+		fac *= i;
+
+	return fac;
+}
+
+double nCr(double n, double r)
+{
+	return nFac(n) / (nFac(n - r) * nFac(r));
+}
+
+double nPr(double n, double r)
+{
+	return nFac(n) / nFac(n - r);
 }
